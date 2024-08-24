e3cf290c9d25cfbc3eceb4acd9f85b03d2fe9808
Jairo Pinheiro Da Conceição Junior <jairopnhr@gmail.com>
2024-08-24 17:40:16 -0300

    Create project

---
+++ backend/my-coffee-shop-back-end/src/main/java/com/example/my_coffee_shop_back_end/MyCoffeeShopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_coffee_shop_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoffeeShopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCoffeeShopBackEndApplication.class, args);
+	}
+
+}
